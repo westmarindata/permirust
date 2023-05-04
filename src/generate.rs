869eb46074e128369519a3830d7a872e88bdfec3
Pedram Navid <pedram.navid@gmail.com>
2023-05-04 10:11:42 -0700

    Add obj permissions by role

--- src/generate.rs
+++ src/generate.rs
@@ -5,11 +5,14 @@ use postgres::Client;
 
 pub fn generate_spec(client: &mut Client) -> Result<()> {
     // Get roles and role attributes
-    get_role_attributes(client);
-    // Get memberships
-    // Get ownerships
-    // Get privileges
-    // Return spec
+    let roles = get_role_attributes(client);
+    let memberships = get_all_memberships(client);
+    let obj_permissions = get_obj_permissions_by_role(client);
+
+    println!("{:?}", roles);
+    println!("{:?}", memberships);
+    println!("{:?}", obj_permissions);
+
     return Ok(());
 }
 
@@ -25,6 +28,21 @@ pub struct PostgresRole {
     replication: bool,
     superuser: bool,
     validuntil: Option<DateTime<Utc>>,
+}
+
+#[derive(Debug)]
+pub struct PostgresMembership {
+    member: String,
+    group: String,
+}
+
+#[derive(Debug)]
+pub struct GranteePrivileges {
+    grantee: String,
+    objkind: String,
+    schema: String,
+    unqualified_name: Option<String>,
+    privilege_type: String,
 }
 
 fn get_role_attributes(client: &mut Client) -> Vec<PostgresRole> {
@@ -56,6 +74,90 @@ fn get_role_attributes(client: &mut Client) -> Vec<PostgresRole> {
             replication: row.get(7),
             superuser: row.get(8),
             validuntil: row.get(9),
+        })
+        .collect()
+}
+
+fn get_all_memberships(client: &mut Client) -> Vec<PostgresMembership> {
+    let query = "SELECT
+            auth_member.rolname AS member,
+            auth_group.rolname AS group
+        FROM pg_auth_members link_table
+        JOIN pg_authid auth_member ON link_table.member = auth_member.oid
+        JOIN pg_authid auth_group ON link_table.roleid = auth_group.oid
+    ";
+
+    let rows = client.query(query, &[]).unwrap();
+
+    rows.iter()
+        .map(|row| PostgresMembership {
+            member: row.get(0),
+            group: row.get(1),
+        })
+        .collect()
+}
+
+fn get_obj_permissions_by_role(client: &mut Client) -> Vec<GranteePrivileges> {
+    let query = "WITH
+    relkind_mapping (objkey, objkind) AS (
+        VALUES ('r', 'tables'),
+               ('v', 'tables'),
+               ('m', 'tables'),
+               ('f', 'tables'),
+               ('S', 'sequences')
+    ), tables_and_sequences AS (
+        SELECT
+            nsp.nspname AS schema,
+            c.relname AS unqualified_name,
+            map.objkind,
+            (aclexplode(c.relacl)).grantee AS grantee_oid,
+            t_owner.rolname AS owner,
+            (aclexplode(c.relacl)).privilege_type
+        FROM
+            pg_class c
+            JOIN pg_authid t_owner ON c.relowner = t_owner.OID
+            JOIN pg_namespace nsp ON c.relnamespace = nsp.oid
+            JOIN relkind_mapping map ON c.relkind = map.objkey
+        WHERE
+            nsp.nspname NOT LIKE 'pg\\_t%'
+            AND c.relacl IS NOT NULL
+    ), schemas AS (
+        SELECT
+             nsp.nspname AS schema,
+             NULL::TEXT AS unqualified_name,
+             'schemas'::TEXT AS objkind,
+             (aclexplode(nsp.nspacl)).grantee AS grantee_oid,
+             t_owner.rolname AS owner,
+             (aclexplode(nsp.nspacl)).privilege_type
+        FROM pg_namespace nsp
+        JOIN pg_authid t_owner
+            ON nsp.nspowner = t_owner.OID
+    ), combined AS (
+        SELECT * FROM tables_and_sequences
+        UNION ALL
+        SELECT * FROM schemas
+    )
+    SELECT
+        t_grantee.rolname AS grantee,
+        combined.objkind,
+        combined.schema,
+        combined.unqualified_name,
+        combined.privilege_type
+    FROM
+        combined
+        JOIN pg_authid t_grantee ON combined.grantee_oid = t_grantee.oid
+        WHERE combined.owner != t_grantee.rolname
+       ";
+
+    let rows = client.query(query, &[]).unwrap();
+
+    rows.iter()
+        .map(|row| GranteePrivileges {
+            grantee: row.get(0),
+            objkind: row.get(1),
+            schema: row.get(2),
+            unqualified_name: row.get(3),
+            privilege_type: row.get(4),
         })
         .collect()
 }
